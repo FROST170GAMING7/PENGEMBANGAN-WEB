0a8ef454ed1045a72e75daa00a597e5c004dc39f
FROST170GAMING7 <frost170gaming7@gmail.com>
2026-01-22 23:24:28 +0700

    Update combined.js

--- combined.js
+++ combined.js
@@ -79,7 +79,7 @@
         });
     }
 
-    // Saat salah satu video diputar, hentikan video lain
+    // Saat salah satu video diputar, hentikan video lainyoutube
     videos.forEach(video => {
         video.addEventListener("play", () => {
             stopOtherVideos(video);
@@ -90,7 +90,7 @@
 //Verify
 function verify() {
     var pass = document.getElementById("passInput").value;
-    var target = "youtube.com/"; // Link yang ingin dibuka
+    var target = "www.youtube.com"; // Link yang ingin dibuka
     
     if (pass === "RAHASIA") {
         window.location.href = target;
@@ -98,3 +98,4 @@ function verify() {
         alert("Password salah!");
     }
 }
+
